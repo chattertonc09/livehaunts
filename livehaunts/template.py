8ef4682dfd72641aa508471c1cf8148b8f79f0f9
root <root@LAPTOP-CFOIV878>
2025-01-02 16:58:09 -0500

    adds app template

---
+++ livehaunts/template.py
@@ -0,0 +1,21 @@
+from typing import Callable
+
+import reflex as rx
+
+from livehaunts.components import footer, navbar
+
+
+def template(page: Callable[[], rx.Component]) -> rx.Component:
+    return rx.desktop_only(
+        rx.vstack(
+            navbar(),
+            page(),
+            rx.spacer(),
+            footer(),
+            width="100%",
+            height="100vh",
+        ),
+        width="100%",
+        height="100vh",
+        color_scheme="violet",
+    )
