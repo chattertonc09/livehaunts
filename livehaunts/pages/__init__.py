5c2c7f068822a96f5070aeb2e46d01871ef2ee2b
root <root@LAPTOP-CFOIV878>
2025-01-02 16:59:46 -0500

    adds index page

---
+++ livehaunts/pages/__init__.py
@@ -0,0 +1,5 @@
+from livehaunts.pages import index
+
+__all__ = [
+    "index",
+]
---
+++ livehaunts/pages/index.py
@@ -0,0 +1,47 @@
+import reflex as rx
+
+from livehaunts.template import template
+
+
+def top_section() -> rx.Component:
+    return rx.vstack(
+        rx.divider(),
+        rx.heading("Subcribe", size="1", align="center", width="100%"),
+        rx.html(
+            """
+            <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
+            <stripe-pricing-table pricing-table-id="prctbl_1QcECYR3TXEezMNquWOIqoJi"
+            publishable-key="pk_test_51Qb623R3TXEezMNqMrVqN7bwb7keBQKZbKOOhS4Ny3Fb0Tnac2hJ63ek5y8yoplqTdvermVuv9qTJCtOFsP4uJ1700MGyfSPK1">
+            </stripe-pricing-table>
+            """,
+            align="center",
+            width="100%",
+        ),
+        width="100%",
+        padding_top="5em",
+    )
+
+
+def mid_section() -> rx.Component:
+    return rx.hstack(width="100%")
+
+
+def bottom_section() -> rx.Component:
+    return rx.hstack(width="100%")
+
+
+def layout() -> rx.Component:
+    return rx.vstack(
+        top_section(),
+        mid_section(),
+        bottom_section(),
+        width="100%",
+    )
+
+
+@template
+def index():
+    return rx.vstack(
+        layout(),
+        width="100%",
+    )
